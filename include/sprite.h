7aec7f0a0b95f786ab691c763a0a79673a79af84
RS2 <96263419+juice2011@users.noreply.github.com>
2024-04-18 07:16:10 -0400

    Update sprite.h
    This was made on my phone

--- include/sprite.h
+++ include/sprite.h
@@ -1,11 +1,23 @@
 #pragma once
 #include "../include/TS_class.h"
 
+class Texture {
+public:
+	  char* location;
+			int glLocation;
+			// Load Texture is not implemented yet
+			// operator char*() const { return LoadTexure(location) }
+};
+
 class Sprite {
-	float x, y, z;
-
-	Sprite()
-	{
-
-	}
+public:
+		float x, y, z;
+  
+		Sprite()
+		{
+					
+		}
+		
+private:
+	 void CreateVertexBuffer();
 };
